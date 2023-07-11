055140a6ee6bf81e4a4cd054a9c3fdff5f7cbe01
irwanphan <irwanphan@gmail.com>
2023-07-11 16:02:41 +0700

    iterate cards inside of content area

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -4,41 +4,60 @@ const Home = {
     },
    
     async _initialData() {
+        this._userStory = null;
+
         const fetchRecords = await fetch('/data/DATA.json');
         const responseRecords = await fetchRecords.json()
-            .then((data) => console.log(data));
-    },
-        // this._userTransactionsHistory = responseRecords.results.transactionsHistory;
+            .then((data) => this._userStory = data.listStory);
+        // console.log(this._userStory);
         // this._populateTransactionsRecordToTable(this._userTransactionsHistory);
-        // this._populateTransactionsDataToCard(this._userTransactionsHistory);
+        this._populateStoryDataToCard(this._userStory);
+    },
    
-    // _populateTransactionsDataToCard(transactionsHistory = null) {
-    //     if (!(typeof transactionsHistory === 'object')) {
-    //         throw new Error(
-    //             `Parameter responseRecords should be an object.`,
-    //         );
-    //     }
+    _populateStoryDataToCard(stories = null) {
+        if (!(typeof stories === 'object')) {
+            throw new Error(
+                `Parameter responseRecords should be an object.`,
+            );
+        }
+
+        if (!Array.isArray(stories)) {
+            throw new Error('Parameter stories should be an array.');
+        }
+
+        const recordStories = document.querySelector('#recordStories');
    
-    //     if (!Array.isArray(transactionsHistory)) {
-    //         throw new Error('Parameter transactionsHistory should be an array.');
-    //     }
-   
-    //     let amountIncome = 0;
-    //     let amountExpense = 0;
-   
-    //     transactionsHistory.forEach((item) => {
-    //         if (item.type === 'income') {
-    //             amountIncome += item.amount;
-    //         } else if (item.type === 'expense') {
-    //             amountExpense += item.amount;
-    //         }
-    //     });
-   
-    //     document.querySelector('#numberOfTransactions').innerText =
-    //         transactionsHistory.length;
-    //     document.querySelector('#amountIncome').innerText = amountIncome;
-    //     document.querySelector('#amountExpense').innerText = amountExpense;
-    // },  
+        recordStories.innerHTML = '';
+        if (stories.length <= 0) {
+            recordStories.innerHTML = this._templateEmptyStory();
+            return;
+        }
+        stories.forEach((item) => {
+            recordStories.innerHTML += this._templateStoryCard(item);
+        });
+    },
+
+    _templateStoryCard(story) {
+        return `
+            <div class="col-12 col-sm-6 col-lg-4">
+                <div class="card bordered h-100 bg-gradient">
+                    <div class="card-body">
+                        <h1 class="card-title fs-2">
+                            <span>card item</span>
+                        </h1>
+                        <p class="card-text"></p>
+                    </div>
+                </div>
+            </div>
+        `
+    },
+
+    _templateEmptyStory() {
+        const recordStories = document.querySelector('#recordStories');
+        return `
+            <div class="text-center">No story to show</div>
+        `;
+    },
 };
    
 export default Home;
