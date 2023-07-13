ae67db00f4fae30c3d946d78fe0db2683dd34ccc
irwanphan <irwanphan@gmail.com>
2023-07-13 20:41:43 +0700

    close button custom

--- src/js/components/off-canvas.js
+++ src/js/components/off-canvas.js
@@ -30,7 +30,7 @@ class OffCanvas extends LitElement{
                 aria-labelledby="staticBackdropLabel">
                 <div class="offcanvas-header">
                     <h5 class="offcanvas-title" id="staticBackdropLabel">Story App</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"><i class="bi bi-x-circle"></i></button>
                 </div>
                 <div class="offcanvas-body">
                     <div class="offcanvas-nav">
