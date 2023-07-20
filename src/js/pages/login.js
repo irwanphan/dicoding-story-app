4ad19e51488586b21ccbb298354e25305988ab30
irwanphan <irwanphan@gmail.com>
2023-07-20 10:05:58 +0700

    interactive form invalid message and loading button

--- src/js/pages/login.js
+++ src/js/pages/login.js
@@ -26,18 +26,17 @@ const Login = {
             async (event) => {
                 event.preventDefault();
                 event.stopPropagation();
-
                 loginForm.classList.add('was-validated');
-                loginButton.setAttribute('disabled', true);
-                loginButton.setAttribute('isLoading', true);
                 try {
+                    loginButton.setAttribute('disabled', true);
+                    loginButton.setAttribute('isLoading', true);
                     await this._handleLoginSubmit();
-                    loginButton.setAttribute('isLoading', '');
-                    loginButton.setAttribute('disabled', '');
-                } catch (error) {
                     loginButton.removeAttribute('isLoading');
                     loginButton.removeAttribute('disabled');
+                } catch (error) {
                     console.error(error);
+                    loginButton.removeAttribute('isLoading');
+                    loginButton.removeAttribute('disabled');
                 }
             },
             false,
@@ -46,21 +45,41 @@ const Login = {
 
     async _handleLoginSubmit() {
         const formData = this._getFormData();
+        // Remove shown error messages
+        const emailInput = document.querySelector('#email');
+        const passwordInput = document.querySelector('#password');
+        emailInput.classList.remove('is-invalid');
+        passwordInput.classList.remove('is-invalid');
 
         if (this._validateFormData({ ...formData })) {
-            console.log('formData', formData);
-        }
-
-        try {
-            const response = await Auth.login({
-                email: formData.email,
-                password: formData.password,
-            });
-            Utils.setUserToken(Config.USER_TOKEN_KEY, response.data.loginResult.token);
-            window.alert('Signed user in detected');
-            this._goToDashboardPage();
-        } catch (error) {
-            console.error(error);
+            try {
+                const response = await Auth.login({
+                    email: formData.email,
+                    password: formData.password,
+                });
+                Utils.setUserToken(Config.USER_TOKEN_KEY, response.data.loginResult.token);
+                window.alert('Signed user in detected');
+                this._goToDashboardPage();
+            } catch (error) {
+                console.error(error);
+                // Handle the error response, show the appropriate error message, and mark the invalid fields
+                if (error.response && error.response.data && error.response.data.message) {
+                    const errorMessage = error.response.data.message;
+                    if (errorMessage.includes('email')) {
+                        emailInput.classList.add('is-invalid');
+                    }
+                    if (errorMessage.includes('password')) {
+                        passwordInput.classList.add('is-invalid');
+                    }
+                }
+            }
+        } else {
+            if (!formData.email) {
+                emailInput.classList.add('is-invalid');
+            }
+            if (!formData.password) {
+                passwordInput.classList.add('is-invalid');
+            }
         }
     },
 
